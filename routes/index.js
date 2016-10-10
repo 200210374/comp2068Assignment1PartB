aa68dabc6737604af767f5e08483e6066dcc00b0
brandonroycstn <brandonroycstn@hotmail.com>
2016-10-09 21:24:59 -0600

    Attempted fixes part1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'home' });
+});
+
+
+/* Contact page */
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+
+
 module.exports = router;
+
+
+
