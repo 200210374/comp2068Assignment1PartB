8cdfa4ac5fd3f98eb0a6e19b195e94887ee1e4db
brandonroycstn <brandonroycstn@hotmail.com>
2016-10-12 13:21:36 -0600

    Added server.js

--- server.js
+++ server.js
@@ -1,4 +1,65 @@
-'use strict';
+
+
+
+var express = require('express');
+var app = express();
+app.use('/', function(req, res) {
+    res.send('Hello World');
+});
+app.listen(3000);
+console.log('Server running at http://localhost:3000/');
+module.exports = app;
+
+
+
+
+
+
+/*
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+
+app.get('/contact', function(req, res) {
+    res.render('pages/contact');
+});
+
+
+app.get('/about', function(req, res) {
+    res.render('pages/projects');
+});
+
+app.get('/about', function(req, res) {
+    res.render('pages/services');
+});
+
+app.get('/about', function(req, res) {
+    res.render('pages/home');
+});
+
+
+
+app.listen(8080);
+
+
+*/
+
+/*
 
 // ================================================================
 // get all the tools we need
@@ -25,4 +86,4 @@ routes(app);
 // ================================================================
 app.listen(port, function() {
     console.log('Server listening on port ' + port + '…');
-});
+}); */
