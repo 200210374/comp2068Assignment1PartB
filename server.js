e779a409bc7aea5e088ac390119e124c0889187d
brandonroycstn <brandonroycstn@hotmail.com>
2016-10-12 16:25:51 -0600

    added the partial header to index.js

--- server.js
+++ server.js
@@ -26,14 +26,14 @@ app.get('/about', function(req, res) {
 app.get('/contact', function(req, res) {
     res.render('pages/contact');
 });
-app.get('/about', function(req, res) {
+app.get('/projects', function(req, res) {
     res.render('pages/projects');
 });
-app.get('/about', function(req, res) {
+app.get('/home', function(req, res) {
     res.render('pages/home');
 });
 
-app.get('/about', function(req, res) {
+app.get('/services', function(req, res) {
     res.render('pages/services');
 });
 
@@ -44,39 +44,3 @@ app.listen(3000);
 console.log('Port 3000');
 
 
-/*
-
-var express = require('express');
-var app = express();
-app.use('/', function(req, res) {
-    res.send('Hello World');
-});
-
-module.exports = function(app) {
-    app.get('/', function(req, res) {
-        res.render('pages/index');
-    });
-
-    app.get('/about', function(req, res) {
-        res.render('pages/about');
-    });
-
-
-    app.get('/contact', function(req, res) {
-        res.render('pages/contact');
-    });
-
-    app.get('/Projects', function(req, res) {
-        res.render('pages/Projects');
-    });
-
-    app.get('/Services', function(req, res) {
-        res.render('pages/Services');
-    });
-
-
-
-
-app.listen(3000);
-console.log('Server running at http://localhost:3000/')};
-//module.exports = app; */
