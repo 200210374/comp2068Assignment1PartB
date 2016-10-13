2726edd3ef277da371b239a2c08136e7489525f1
brandonroycstn <brandonroycstn@hotmail.com>
2016-10-13 14:25:14 -0600

    attempt to fix controller and project2

--- routes/contact.js
+++ routes/contact.js
@@ -1,3 +1,14 @@
 /**
  * Created by Brandon Roy on 13/10/2016.
  */
+
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+    res.render('contact', { title: 'contact' });
+});
+
+
+module.exports = router;
